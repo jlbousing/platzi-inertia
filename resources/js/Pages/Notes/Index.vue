1ad966961fd410fba9aea55acd6c15a69bbf34c1
jorge <jbousing@gmail.com>
2022-12-12 16:53:01 -0400

    listado de notas: personalizacion

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -1,5 +1,9 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    notes: Array
+})
 </script>
 
 <template>
@@ -17,6 +21,27 @@ import AppLayout from '@/Layouts/AppLayout.vue';
                         <div class="px-4 sm:px-0">
                             <h3 class="text-lg text-gray-900">Listado de notas</h3>
                             <p class="text-sm text-gray-600">Toma el registro correcto y ejecuta cualquier funcion (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded">
+                            <table>
+                                <tr v-for="(note,i) in notes" :key="i">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.show',note.id)">
+                                            Ver
+                                        </inertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.edit',note.id)">
+                                            Editar
+                                        </inertia-link>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
