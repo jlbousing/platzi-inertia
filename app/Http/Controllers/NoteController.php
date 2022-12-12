7636cda6a23088220e7494d60f2cbb9c6811e116
jorge <jbousing@gmail.com>
2022-12-12 13:44:13 -0400

    configuracion inicial de listado de notas

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class NoteController extends Controller
 {
@@ -14,7 +15,9 @@ class NoteController extends Controller
      */
     public function index()
     {
-
+        return Inertia::render("Notes/Index",[
+            "notes" => Note::latest()->get()
+        ]);
     }
 
     /**
---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">Listado de notas</h3>
+                            <p class="text-sm text-gray-600">Toma el registro correcto y ejecuta cualquier funcion (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
