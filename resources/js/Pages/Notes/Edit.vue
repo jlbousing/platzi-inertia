4245048216c7b03b22170d5d2474b2e9eb98a762
jorge <jbousing@gmail.com>
2022-12-12 17:42:49 -0400

    formulario de editar

---
+++ resources/js/Pages/Notes/Edit.vue
@@ -0,0 +1,70 @@
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">Editar nota</h3>
+                            <p class="text-sm text-gray-600">Si editas, no podras volver al estado anterior</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded">
+                            <form>
+                                <label class="block font-medium text-sm text-gray-700">
+                                    Resumen
+                                </label>
+                                <textarea
+                                    class="form-input w-full rounded-md shadow-sm"
+                                    v-model="form.excerpt"
+                                ></textarea>
+                                <label class="block font-medium text-sm text-gray-700">
+                                    Contenido
+                                </label>
+                                <textarea
+                                    class="form-input w-full rounded-md shadow-sm"
+                                    v-model="form.content"
+                                    rows="8"
+                                ></textarea>
+                                <button
+                                    class="font-bold py-2 px-4 rounded-md bg-blue-500"
+                                >Editar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import {Link} from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        AppLayout,
+        Link
+    },
+    props: {
+        note: Object
+    },
+    data() {
+        return {
+            form: {
+                excerpt: this.note.excerpt,
+                content: this.note.content
+            }
+        }
+    }
+}
+</script>
