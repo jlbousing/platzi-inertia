0070565ef900c763582f1d272149b10575be6868
jorge <jbousing@gmail.com>
2022-12-13 10:19:58 -0400

    buscador

--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -1,10 +1,27 @@
-<script setup>
+<script>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import {Link} from "@inertiajs/inertia-vue3";
 
-defineProps({
-    notes: Array
-})
+export default {
+    components: {
+        AppLayout,
+        Link
+    },
+    props: {
+        notes: Array
+    },
+    data() {
+        return {
+            q: ''
+        }
+    },
+    watch: {
+        q: function (value) {
+            this.$inertia.replace(this.route('notes.index',{q: value}))
+        }
+    }
+}
+
 </script>
 
 <template>
@@ -25,9 +42,18 @@ defineProps({
                         </div>
                     </div>
                     <div class="md:col-span-2 mt-5 md:mt-0">
-                        <div class="shadow bg-white md:rounded">
-                            <Link :href="route('notes.create')"
-                                  class="bg-blue-500  font-bold px-4 py-2 rounded">Crear</Link>
+                        <div class="shadow bg-white md:rounded p-4">
+                            <div class="flex justify-between">
+                                <input type="text"
+                                       class="form-input rounded-md shadow-sm"
+                                       placeholder="buscar..."
+                                       v-model="q">
+                                <Link :href="route('notes.create')"
+                                      class="bg-blue-500  font-bold px-4 py-2 rounded">
+                                    Crear
+                                </Link>
+                            </div>
+                            <hr class="my-6" />
                             <table>
                                 <tr v-for="(note,i) in notes" :key="i">
                                     <td class="border px-4 py-2">
